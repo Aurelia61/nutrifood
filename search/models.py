78c4a1631f718059b8f4b2d9e0a491a0a1fa786e
Aurelia61 <67269542+Aurelia61@users.noreply.github.com>
2020-11-18 11:43:19 +0100

    model migrate ok

--- search/models.py
+++ search/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name_product = models.CharField(max_length=200)
+
+
+class Ingredient(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    name_ingredient = models.CharField(max_length=200)
+    quantity = models.IntegerField(default=0)
